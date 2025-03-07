1f440fbc4ca26957a5992ec1284c40e17d381989
LalithKatta <31791001+lalithkatta9@users.noreply.github.com>
2025-03-07 11:58:11 -0600

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "RadarSDK",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "RadarSDK",
+            targets: ["RadarSDK"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "RadarSDK",
+            path: "RadarSDK",
+            exclude: ["Info.plist"],
+            resources: [.process("PrivacyInfo.xcprivacy")],
+            publicHeadersPath: "Include",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+        ),
+    ]
+)
